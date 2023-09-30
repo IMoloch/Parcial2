8bcae7a1c18071bba5532adc32bef8eeb0ee307f
Javier <javierivera29@gmail.com>
2023-09-30 17:53:28 -0600

    Inicialización de proyecto

---
+++ database/migrations/2023_09_30_224444_create_alumnos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->id();
+            $nombre->string();
+            $apellido->string();
+            $fecha_nacimiento->date();
+            $direccion->string();
+            $genero->string(1);
+            $telefono->int();
+            $correo->string();
+            $clave->$string();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alumnos');
+    }
+};
